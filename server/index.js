750228e1c7304e51d342242a17183e6b99d0e597
Sofia Grigoreva <sofya.grigorevaa@gmail.com>
2025-09-09 12:05:45 +0300

    refactor: architecture changed

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '..'))); 
 
 app.get('/', (_, res) => {
-  res.sendFile(path.resolve('public', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
 
 app.listen(2727, () => {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './build/precompiled.js';
 
-import Home from './src/pages/home/home.js';
+import Home from './pages/home/home.js';
 
 const root = document.createElement('div');
 root.id = 'root';
